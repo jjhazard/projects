b2e8005413395a251bdb5b4602d633c49ac50370
jjhazard <47158291+jjhazard@users.noreply.github.com>
2020-06-21 12:40:19 -0400

    Add files via upload

---
+++ CSC402/Assignment3_jh/jh1_lrinterp_gram.py
@@ -0,0 +1,9 @@
+from jh1_lrinterp_gram import parser
+
+def jh1_lrinterp(input_stream = None):
+    'A driver for our LR Exp1 interpreter.'
+    
+    if not input_stream:
+        input_stream = input("exp1 > ")
+    
+    parser.parse(input_stream)
